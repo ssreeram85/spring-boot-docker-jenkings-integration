646ce7f3f334b032f4edb1350d305fd7eb5e673a
sreeram85 <sanepalli.sreeram@gmail.com>
2021-05-28 12:11:15 +0530

    initial commit

---
+++ src/main/java/com/core/it/springbootdockerjenkingsintegration/SpringBootDockerJenkingsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.core.it.springbootdockerjenkingsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerJenkingsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerJenkingsIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/core/it/springbootdockerjenkingsintegration/SpringBootDockerJenkingsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.it.springbootdockerjenkingsintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerJenkingsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
